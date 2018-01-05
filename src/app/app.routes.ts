1829d64e89498e656238d7823ce7e1d209a4dcb2
Andres <andresbrusutti@gmail.com>
2018-01-05 07:23:50 -0300

    Proyecto Angular Avanzado

---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by abrusutt on 17/10/2017.
+ */
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const APP_ROUTES: Routes = [
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent},
+      { path: 'graficas1', component: Graficas1Component},
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+    ]
+  }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NopagefoundComponent}
+];
+
+export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES, { useHash: true });
+
